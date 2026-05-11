3071f274f87e631faefa44e62e5cbce1fe03be83
Michael <michaelquint@gmail.com>
2026-05-11 19:34:27 -0400

    some javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Week 4 Javascript");
+/*
+let age = 34;
+
+if( age >= 18) {
+    console.log("Congrats. You are old enough");
+}
+else {
+    console.log("Sorry, try again later");
+}
+
+
+let password = "12345";
+
+if(password == "12345") {
+    alert("Congratulations. You have passed the test");
+    console.log('successful attempt');
+}
+else {
+    alert("Incorrect password")
+    console.log("failed attempt.");
+}
+    */
+
+
+function checkage() {
+    let age = document.getElementById("age").value;
+
+    if(age >= 18) {
+        alert("you are old enough");
+    }
+    else{
+        alert("You are NOT old enough");
+    }
+}
